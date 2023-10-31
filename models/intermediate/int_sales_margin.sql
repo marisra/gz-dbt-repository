44f25cbc60ac0b35e748e9fc288b87e0f7c4b652
Maris Raudlam <marisra.dbt@gmail.com>
2023-10-31 19:23:42 +0000

    oo

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,8 @@ SELECT
     quantity, 
     purchase_price, 
     ROUND(CAST(s.quantity as float64)*CAST(p.purchase_price as float64),2) AS purchase_cost,
-    CAST(s.revenue as float64) - ROUND(CAST(s.quantity as float64)*CAST(p.purchase_price as float64),2) AS margin
+    CAST(s.revenue as float64) - ROUND(CAST(s.quantity as float64)*CAST(p.purchase_price as float64),2) AS margin,
+    {{ margin_percent('s.revenue', 's.quantity*CAST(p.purchase_price AS FLOAT64)') }} as margin_percent 
 FROM {{ref("stg_raw__sales")}} s
 LEFT JOIN {{ref("stg_raw__product")}} p 
 	USING (products_id)
